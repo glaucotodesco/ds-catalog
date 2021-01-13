43f01b10eda69df8f225aad106adae33a252cb3b
glauco.todesco <glauco.todesco@facens.br>
2021-01-13 18:10:29 -0300

    Add CategoryRepository

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -3,20 +3,25 @@ package com.devsuperior.dscatalog.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryResource {
 	
+	@Autowired
+	private CategoryService categoryService;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> categories = new ArrayList<>();
+		List<Category> categories = categoryService.findAll();
 		return ResponseEntity.ok().body(categories);
 	}
 	
