2851acdda290384c23d5249486cbb15f251cbaab
glauco.todesco <glauco.todesco@facens.br>
2021-01-28 22:48:51 -0300

    Finish Layout Product

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import ProductCard from './components/ProductCard';
+import './styles.scss';
 
 const Catalog = () => (
-    <div>
-        <h1>Catalog</h1>
+    <div className="catalog-container">
+        <h1 className="catalog-title">
+            Catálago de Produtos
+        </h1>
+        <div className="catalog-products">
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+        </div>
     </div>
+
 
 );
 export default Catalog;
