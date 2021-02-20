bff0181469efef4c7f72361b009a2c23f9a28bfe
glauco.todesco <glauco.todesco@facens.br>
2021-02-20 13:46:09 -0300

    Ready To Prod

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -16,17 +19,23 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     private TokenStore tokenStore;
 
-    private static final String[] PUBLIC = { "/oauth/token" };
-                                                                             
-    private static final String[] OPERATOR_OR_ADMIN = { "/products/**","/categories/**" };
+    @Autowired
+    private Environment env;
+
+    private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+
+    private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 
     private static final String[] ADMIN = { "/users/**" };
 
-
-
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
         
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
         //@formatter:off
         http.authorizeRequests()
             .antMatchers(PUBLIC).permitAll()
