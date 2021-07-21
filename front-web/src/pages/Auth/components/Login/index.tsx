fdb2f39c2890c9c3c8608b4cf5c44b4d919b5d24
Glauco Todesco <glauco.todesco@gmail.com>
2021-07-21 16:00:21 -0300

    Login with post to backend

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,19 +1,51 @@
 import ButtonIcon from 'core/components/Buttonicon';
+import { makeLogin } from 'core/utils/request';
+import { useState } from 'react';
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Link } from 'react-router-dom';
 import AuthCard from '../Card';
 import './styles.scss';
 
+type Inputs = {
+    username: string,
+    password: string,
+};
+
 const Login = () => {
+
+    const [hasError, setHasError] = useState(false);
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = data => {
+        makeLogin(data)
+        .then( response => {
+            setHasError(false)
+        })
+        .catch( () => setHasError(true))
+    }
+
     return (
         <AuthCard title="Login">
-            <form className="login-form">
+           
+            {hasError && (
+                <div className="alert alert-danger mt-5">
+                    Usuário ou senha inválidos!
+                </div>
+            )}
+
+            
+
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <input
                     type="email"
+                    {...register("username", { required: true })}
                     className="form-control input-base margin-bottom-30"
                     placeholder="E-mail"
                 />
                 <input
                     type="password"
+                    {...register("password",{ required: true })}
                     className="form-control input-base"
                     placeholder="Senha"
                 />
@@ -21,9 +53,9 @@ const Login = () => {
                     Esqueci a senha?
                 </Link>
                 <div className="login-submit">
-                    <ButtonIcon text="Logar"/>
+                    <ButtonIcon text="Logar" />
                 </div>
-                <div className="text-center"> 
+                <div className="text-center">
                     <span className="not-registered">Não tem cadastro</span>
                     <Link to="/admin/auth/register" className="login-link-register">
                         CADASTRAR
