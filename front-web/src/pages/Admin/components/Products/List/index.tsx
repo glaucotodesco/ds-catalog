0ce1df6c439997e07a33a51499550536ed173de5
Glauco Todesco <glauco.todesco@gmail.com>
2021-07-28 19:33:01 -0300

    Admin Product List

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,8 +1,35 @@
+import Pagination from "core/components/Pagination";
+import { ProductsResponse } from "core/types/Products";
+import { makeRequest } from "core/utils/request";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import Card from "../Card";
 
 const List  = () => {
+
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
+    const linesPerPage: number = 4;
+    
+    useEffect(() => {
+
+        const params = {
+            page: activePage,
+            linesPerPage: linesPerPage
+        };
+
+        setIsLoading(true);
+        makeRequest({ url: '/products', params })
+            .then(response => setProductsResponse(response.data))
+            .finally(() => { setIsLoading(false) })
+
+    }, [activePage]);
+
     
     const history =  useHistory();
+
+    console.log(productsResponse);
 
     const handleCreate = () => {
         history.push('/admin/products/create');
@@ -13,6 +40,24 @@ const List  = () => {
             <button className="btn btn-primary btn-lg" onClick={handleCreate}>
                 ADICIONAR
             </button>
+
+            <div className="admin-list-container">
+                {productsResponse?.content.map(product => ( 
+                                            <Card product={product} key={product.id}  />   
+                                                
+                                            )
+                                        )
+                }
+                 {
+                productsResponse && 
+                    <Pagination 
+                        totalPages={productsResponse.totalPages}
+                        activePage={activePage}
+                        onChange={page => setActivePage(page)}
+                    />
+            }
+            </div>
+
         </div>
     );
 }
--- front-web/src/pages/Admin/components/Products/index.tsx
+++ front-web/src/pages/Admin/components/Products/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route, Switch } from 'react-router-dom';
 import Form from './Form';
 import List from './List';
-import './styles.scss';
+
 
 
 const Products = () => {
